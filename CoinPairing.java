14f865060c8185974fd4e01ee73107b861cc1c6d
fa7tony <barnes.c.ethan@gmail.com>
2018-11-12 14:09:30 +0000

    Add files via upload

---
+++ CoinPairing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coinpairing;
+/**
+ *
+ * @author Ethan
+ */
+public class CoinPairing {
+    public static void main(String[] args) {
+        Problem4 test = new Problem4();
+        test.numberOfCoins();
+        if (test.coins % 2 == 0) {
+            test.even();
+        } else {
+            test.odd();
+        }
+        test.coinsOutput();
+    }
+}
