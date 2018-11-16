fd8afeea274da77c071ab1bdb46b8644b94f7661
fa7tony <barnes.c.ethan@gmail.com>
2018-11-16 18:03:52 +0000

    Final code

--- CoinPairing/src/coinpairing/CoinPairing.java
+++ CoinPairing/src/coinpairing/CoinPairing.java
@@ -1,13 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package coinpairing;
-
 /**
  *
- * @author Ethan
+ * @author ethanbarnes
  */
 public class CoinPairing {
-
     public static void main(String[] args) {
+        Problem4 test = new Problem4();
+        test.numberOfCoins();
+        if (test.coins % 2 == 0) {
+            test.even();
+        } else {
+            test.odd();
+        }
+        test.coinsOutput();
         
+       
     }
-    
 }
